4fb1743423fd80528d29681247d75f0dceeb43e8
ksngo <sevenelectrons@gmail.com>
2020-03-27 16:21:09 +0800

    check on crossfilter, leaflet, csvtojson

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,69 @@
 
-alert('you are in index.js')
+$(function(){
+
+    axios.get('data/resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.csv').then(function(response){
+
+        
+        csv().fromString(response.data).then(function(jsonData){
+            
+            // console.table(jsonData)
+
+            let cf=crossfilter(jsonData)  //apply crossfilter to jsonData
+
+            let townDimension = cf.dimension(jsonData=>jsonData.town) //select town as dimension
+            // month pending 2017-01
+            let flatTypeDimension = cf.dimension(jsonData=>jsonData.flat_type)
+            let streetNameDimension = cf.dimension(jsonData=>jsonData.street_name)
+            let storeyRangeDimension = cf.dimension(jsonData=>jsonData.storey_range)
+            let floorAreaSqmDimension = cf.dimension(jsonData=>jsonData.floor_area_sqm)
+            let flatModelDimension = cf.dimension(jsonData=>jsonData.flat_model)
+            // remaining lease pending 61 years 04 months
+            let priceDimension = cf.dimension(jsonData=>jsonData.resale_price) // select town as price
+        
+            // console.table(townDimension.top(20))
+            // console.log(townDimension.top(10)[0].resale_price)
+
+            $('#buttonTopPrice').click(topPriceFn)
+
+            function topPriceFn() {
+                
+                let userInputTown = $('#inputTown').val()
+                
+                
+                townDimension.filter(function(town){ //filter town dimension 
+                    return town==userInputTown
+                })
+
+                // console.log('bugging')
+                // console.table(townDimension.top(50))
+                // console.table(priceDimension.top(50))
+                console.table(floorAreaSqmDimension.top(50))
+
+                
+
+                if (userInputTown ='ANG MO KIO') {
+                    
+
+
+                    L.marker([1.369115,103.845436]).addTo(mymap).bindPopup('<p>AMK</p>')
+
+                } else {
+                    alert ('Marker except for ANG MO KIO is not ready')
+                }
+
+            }
+
+            
+
+
+        })// end csvtoJson
+
+
+
+
+
+    }) // end axios.get
+
+
+}) //end generic function
+
--- javascript/map.js
+++ javascript/map.js
@@ -1,12 +1,22 @@
 
-let mymap =L.map('map').setView([1.35,103.85],11)
+let mymap = L.map('map').setView([1.35, 103.85], 11)
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoia3NuZ28iLCJhIjoiY2s4OW1hN2VmMDF5eTNpbTFtMnc3bHU4cyJ9.LvZap-kPuG2evKJHeCHsRA'
-}).addTo(mymap);
+$(function () {
+
+    
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoia3NuZ28iLCJhIjoiY2s4OW1hN2VmMDF5eTNpbTFtMnc3bHU4cyJ9.LvZap-kPuG2evKJHeCHsRA'
+    }).addTo(mymap);
+
+    
+
+
+})
+
 

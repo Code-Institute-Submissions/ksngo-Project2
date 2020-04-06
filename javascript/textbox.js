496bab4ca5b78751809cb2bb1edbc13a69d36ebe
ksngo <sevenelectrons@gmail.com>
2020-04-06 15:17:21 +0800

    editing css styles for text boxes

---
+++ javascript/textbox.js
@@ -0,0 +1,16 @@
+/****************************https://codepen.io/Azuk/pen/wudbk**************************** */
+
+$(".textbox").focus(function(){
+      
+    $(this).animate({backgroundColor : '#0077ff'})
+    $(this).css('color','white')
+    
+    // if($(this).val() ==''){
+    //     $(this).css('background-color', 'white')
+    //     $(this).css('color','black')
+        
+    // }
+    
+    
+  });
+  
